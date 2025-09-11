4e27e7a0c7d630b67b9b9f52085dae73f5cc85ce
Yahweh's Delight <michealphenh10@gmail.com>
2025-09-11 04:01:15 +0300

    All Users Logged In are Staff by default

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -7,6 +7,9 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
 
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", False)
+
         user = self.model(email=email, **extra_fields)
 
         user.set_password(password)
