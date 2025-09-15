78241d088b460c3f40de3981eff8e78bf923fde9
Yahweh's Delight <michealphenh10@gmail.com>
2025-09-16 00:39:56 +0300

    Deploy fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tagger()],
   base: '/',
